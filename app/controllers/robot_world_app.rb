5f51002d67670d41e208cc6ee325a37b5c44fdea
Ashley Schauer <aelschauer@gmail.com>
2017-01-24 17:14:33 -0700

    adds render for all robots. adds view for robots/new.

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -4,7 +4,20 @@ class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
   get '/robots' do
+    @robots = Robot.all
     erb :index
   end
+
+  get '/' do
+    redirect '/robots'
+  end
+
+  get '/robots/new' do
+    @states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    erb :new
+  end
+
+
+
 end
 
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,8 +1,25 @@
 require 'sqlite3'
 
 class Robot
+
   def initialize(robot_params)
-    @database = SQLite3::Database.new('db/robot_world_development.db')
-    @database.results_as_hash = true
+    @database   = Robot.database
+    @id         = robot_params["id"]
+    @name       = robot_params["name"]
+    @city       = robot_params["city"]
+    @state      = robot_params["state"]
+    @department = robot_params["department"]
   end
+
+  def self.database
+    database = SQLite3::Database.new('db/robot_world.db')
+    database.results_as_hash = true
+    database
+  end
+
+  def self.all
+    database
+    database.execute("SELECT * FROM robots")
+  end
+
 end
